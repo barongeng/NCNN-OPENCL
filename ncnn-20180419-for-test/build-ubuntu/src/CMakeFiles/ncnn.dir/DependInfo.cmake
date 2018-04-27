361aaf5f66b2efb905d4bcc3272a5215ee9c5fd0
i am a small sun around you~ <19550021+zhy520xp@users.noreply.github.com>
2018-04-27 12:14:28 +0800

    Add files via upload

---
+++ ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/DependInfo.cmake
@@ -0,0 +1,83 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/benchmark.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/benchmark.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/blob.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/blob.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/cpu.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/cpu.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/absval.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/absval.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/batchnorm.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/batchnorm.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/bias.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/bias.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/binaryop.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/binaryop.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/bnll.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/bnll.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/concat.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/concat.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/convolution.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/convolution.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/convolutiondepthwise.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/convolutiondepthwise.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/crop.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/crop.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/deconvolution.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/deconvolution.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/deconvolutiondepthwise.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/deconvolutiondepthwise.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/detectionoutput.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/detectionoutput.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/dropout.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/dropout.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/eltwise.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/eltwise.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/elu.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/elu.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/embed.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/embed.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/exp.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/exp.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/expanddims.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/expanddims.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/flatten.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/flatten.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/innerproduct.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/innerproduct.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/input.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/input.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/instancenorm.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/instancenorm.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/interp.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/interp.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/log.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/log.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/lrn.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/lrn.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/memorydata.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/memorydata.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/mvn.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/mvn.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/normalize.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/normalize.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/padding.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/padding.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/permute.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/permute.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/pooling.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/pooling.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/power.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/power.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/prelu.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/prelu.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/priorbox.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/priorbox.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/proposal.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/proposal.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/reduction.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/reduction.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/relu.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/relu.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/reshape.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/reshape.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/roipooling.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/roipooling.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/scale.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/scale.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/shufflechannel.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/shufflechannel.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/sigmoid.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/sigmoid.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/slice.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/slice.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/softmax.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/softmax.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/split.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/split.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/squeeze.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/squeeze.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/tanh.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/tanh.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/threshold.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/threshold.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/unaryop.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/unaryop.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/x86/convolution_x86.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/x86/convolution_x86.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/layer/x86/convolutiondepthwise_x86.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/layer/x86/convolutiondepthwise_x86.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/mat.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/mat.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/mat_pixel.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/mat_pixel.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/modelbin.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/modelbin.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/net.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/net.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/opencv.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/opencv.cpp.o"
+  "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/src/paramdict.cpp" "/media/sf_workspace/MTCNN-NCNN-OPENCL/ncnn-20180419-for-test/build-ubuntu/src/CMakeFiles/ncnn.dir/paramdict.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "src"
+  "../src/layer"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
